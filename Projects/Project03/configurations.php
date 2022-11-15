d81d606e853c2c5fabb160dacb09c3dec71dbd33
ChengHung-Wang <apple1314520iphone@icloud.com>
2022-11-15 22:46:10 +0800

    cli debug tools

--- Projects/Project03/configurations.php
+++ Projects/Project03/configurations.php
@@ -1,7 +1,10 @@
 <?php
 namespace Config {
+    require_once "./Color.php";
+    use CLI\Colors;
     use SleekDB\Query;
-    class DB {
+    class DB
+    {
         public static $databaseDirectory = __DIR__ . "/database/";
 
         public static $configuration = array(
@@ -24,5 +27,48 @@ namespace Config {
             "scoreKey" => "scoreKey",
             "algorithm" => Query::SEARCH_ALGORITHM["hits"]
         );
+    }
+
+    class Debug {
+        // true = enable, false = disable
+        public $status = true;
+        private $cli;
+
+        public function __construct()
+        {
+            $this->cli = new Colors();
+        }
+
+        public function error($mess, $end = "\n", $color = "black", $background = "red") {
+            if ($this->status) {
+                echo $this->cli->getColoredString($mess, $color, $background);
+                echo $this->cli->getColoredString($end);
+            }
+            return true;
+        }
+
+        public function success($mess, $end = "\n", $color = "white", $background = "green") {
+            if ($this->status) {
+                echo $this->cli->getColoredString($mess, $color, $background);
+                echo $this->cli->getColoredString($end);
+            }
+            return true;
+        }
+
+        public function info($mess, $end = "\n", $color = "white", $background = "dark_gray") {
+            if ($this->status) {
+                echo $this->cli->getColoredString($mess, $color, $background);
+                echo $this->cli->getColoredString($end);
+            }
+            return true;
+        }
+
+        public function warning($mess, $end = "\n", $color = "black", $background = "yellow") {
+            if ($this->status) {
+                echo $this->cli->getColoredString($mess, $color, $background);
+                echo $this->cli->getColoredString($end);
+            }
+            return true;
+        }
     }
 }
